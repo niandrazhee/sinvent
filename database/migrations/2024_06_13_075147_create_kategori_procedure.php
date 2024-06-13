3911a2b5de13e248a507b7c2e8e1faca603512ba
niandrazhee <niandraamelia@gmail.com>
2024-06-13 14:54:04 +0700

    create kategori procedure

---
+++ database/migrations/2024_06_13_075147_create_kategori_procedure.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+         DB::unprepared('
+        CREATE PROCEDURE bacaKategori()
+        BEGIN
+        SELECT 
+        *
+        FROM
+        kategori
+        ORDER BY kategori ASC;  
+        END
+         ');
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        DB::unprepared('DROP PROCEDURE IF EXISTS bacaKategori');    }
+};
